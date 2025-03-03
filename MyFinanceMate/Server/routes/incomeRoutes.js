3ba3190f420c24ff0c8b77d3ea4473c7e196a13b
HeshaniNiwanthika <heshaniweerasingha042@gmail.com>
2025-03-03 23:01:07 +0530

    income_model

--- MyFinanceMate/Server/routes/incomeRoutes.js
+++ MyFinanceMate/Server/routes/incomeRoutes.js
@@ -4,8 +4,38 @@ let Income=require("../models/income");
 router.route("/add").post((req,res)=>{
     /*get data */
     const description=req.body.description;
-    const amount=req.body.amount;
-    const date=req.body.date;
-    const total_income=req.body.total_income;
-}
-)
+    const amount=Float(req.body.amount);
+    const date=Date(req.body.date);
+    const total_income=Float(req.body.total_income);
+
+
+
+    const newIncome=new Income({
+        description,
+        amount,
+        date,
+        total_income
+    })
+
+    newIncome.save().then(()=>{
+        res.json("Income added")
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+})
+
+router.route("/").get((req,res)=>{
+    
+})
+
+
+
+
+
+
+
+
+
+module.exports=router;
